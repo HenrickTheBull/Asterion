2d275e61e1fee7889d9650aa88d5b80307283822
Bryant Stafford <whitedemon@whitedemon.us>
2020-02-28 05:56:49 -0600

    Some code and style updates. Nothing fancy

--- star.py
+++ star.py
@@ -1,15 +1,33 @@
+#################################
+# 
+#  Main File for Asterion Discord Bot
+#  Written by: Bryant Stafford
+#  Email: bryant.stafford@outlook.com
+#  Discord: HenrickTheBull#1102
+#  
+#  🌟🐂
+#################################
 import discord
 from discord.ext import commands
+import traceback
+import json
+import os
+import asyncio
+import aiohttp
+import random
 
-client = discord.Client()
+client = commands.AutoShardedBot(command_prefix='a.')
 
-client.run()
 
 @client.event
 async def on_ready():
     print('The Stars Call For Me')
+    print("Logged in as %s#%s" % (client.user.name, client.user.discriminator))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+
+
+client.run()
