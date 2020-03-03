5810ffa0f252478f240abd12db35aae624429a19
Bryant Stafford <bryant.stafford@outlook.com>
2020-03-03 08:17:04 -0600

    Bot is broken. Won't load the cogs.

--- star.py
+++ star.py
@@ -16,9 +16,13 @@ import asyncio
 import aiohttp
 import random
 
+print('Loading Primary Config...')
+
 # Reading JSON Settings File
 with open("config.json", "r") as cjson:
     config = json.load(cjson)
+
+print('Loading Bot...')
 
 # Define Bot Client
 client = commands.AutoShardedBot(command_prefix=config["prefix"])
@@ -27,13 +31,29 @@ client = commands.AutoShardedBot(command_prefix=config["prefix"])
 @client.event
 async def on_ready():
     print('The Stars Call For Me.')
-    print("Logged in as %s#%s" % (client.user.name, client.user.discriminator))
+    print('Logged on as {0} (ID: {0.id})'.format(client.user))
+    for cog in cogs:
+        client.load_extension(cog)
 
 # Ignore own Messages.
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+
+
+#Load Cog
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+#Unload Cog
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+print('Loading Cogs...')
+cogs = ['cogs.meta']
 
 # Run The Bot Finally
 client.run(config["token"])
