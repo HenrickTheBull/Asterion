e650f3e71d7e945963657befbe96fef384f0ba57
Bryant Stafford <bstafford@aurochspc.local>
2020-03-27 01:13:23 -0500

    He won't see this. Made a command for him though

---
+++ cogs/fun.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import textwrap
+import urllib.parse
+import random
+import aiohttp
+
+class fun(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['cat', 'randomcat'])
+    async def neko(self, ctx):
+        '''Hey Mitch, wanna see a cat?'''
+        #http://discordpy.readthedocs.io/en/latest/faq.html#what-does-blocking-mean
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('http://aws.random.cat/meow') as r:
+                res = await r.json()
+                emojis = [':cat2: ', ':cat: ', ':heart_eyes_cat: ']
+                await ctx.send(random.choice(emojis) + res['file'])
