177b4363581b989010594082b4908e853986be39
Bryant Stafford <bryant.stafford@outlook.com>
2020-09-22 15:34:41 -0500

    Updates

---
+++ sky/core/functions/configs/config.py
@@ -0,0 +1,24 @@
+import os
+
+from abc import ABC
+from ...utilities import handlers
+
+
+class Config(ABC):
+
+    NAME = str()
+    PATH = str()
+
+    def __init__(self):
+        self.raw = None
+        self.PATH = os.path.join(self.PATH)
+        self._fetch_config()
+
+    def _fetch_config(self):
+        self.raw = handlers.FileHandler.get_file_data(self.PATH)
+        for config in self.raw:
+            if self.raw[config] == "":
+                self.raw[config] = None
+            if config.endswith("_KEY") or config.endswith("_SECRET"):
+                self.raw[config] = os.getenv(config) or self.raw[config]
+            self.__setattr__(config, self.raw[config])
